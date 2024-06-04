3e74e5e36ad53d522bcc134feaf174d1dae2aff1
Muhammad Bilal <muhbil171845@gmail.com>
2024-06-04 18:47:20 +0500

    Add files via upload

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,1 +1,49 @@
+import styles from './ProjectsStyles.module.css';
+import chat from '../../assets/chat.avif';
+import netflix from '../../assets/netflix.avif';
+import ecomm from '../../assets/ecomm.avif';
+import book from '../../assets/book.avif';
+import news from '../../assets/news.avif';
+import ProjectCard from '../../common/ProjectCard';
 
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={chat}
+          link="https://github.com/bilal1718/chatApp"
+          h3="Chatters"
+          p="Chat App"
+        />
+        <ProjectCard
+          src={netflix}
+          link="https://github.com/bilal1718/netflixClone"
+          h3="CineFlex"
+          p="Netflix UI Clone"
+        />
+        <ProjectCard
+          src={ecomm}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Ecommerce"
+          p="Materials Shop"
+        />
+        <ProjectCard
+          src={book}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="BookStore"
+          p="Books App"
+        />
+        <ProjectCard
+          src={news}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="News Explorer"
+          p="News App"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
